33d69db78680c1d5b73e95d765b7d37aee2b4553
Aaron <aaronrobinett@gmail.com>
2021-10-20 12:47:44 -0700

    finished inquirer questions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { getEmployeeData } = require('./lib/userResponses');
 
-console.log('Provide some info about your team and I will generate a webpage for you!');
+console.log('Provide some info about your team and I will generate a webpage for you!\n');
 
 const emplArr = [];
 getEmployeeData(emplArr);
